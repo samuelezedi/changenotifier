1ee8a9348d5c8f93ab76443a296fcbe40b8a0815
Samuel Ezedi <samuelezedi@gmail.com>
2020-06-03 13:05:56 +0100

    upgrades

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -9,21 +9,44 @@ class Settings extends StatelessWidget {
       ),
       body: Column(
         children: <Widget>[
+          SizedBox(height: 25.0,),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text('Units'),
+                DropdownButton<String>(
+                  value: 'Imperial',
+                  onChanged: (String value) {
+                    //TODO UPDATE
+
+                  },
+                  items: <String>['Imperial', 'Metric']
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                        value: value, child: Text(value));
+                  }).toList(),
+                )
+              ],
+            ),
+          ),
           Row(
             children: <Widget>[
-              Text('Units'),
-              DropdownButton<String>(
-                value: 'Imperial',
-                onChanged: (String value) {
-                  //TODO UPDATE
+              Text('Wax Lines'),
+              Container(child: Wrap(
+                spacing: 5.0,
+                runSpacing: 5.0,
+                children: <Widget>[
+                  FilterChip(
+                    label: Text('Swix',style: TextStyle(color: Theme.of(context).accentColor)),
+                    selected: false,
+                    onSelected: (bool value) {
 
-                },
-                items: <String>['Imperial', 'Metric']
-                    .map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem<String>(
-                      value: value, child: Text(value));
-                }).toList(),
-              )
+                    },
+                  )
+                ],
+              ),)
             ],
           )
         ],
